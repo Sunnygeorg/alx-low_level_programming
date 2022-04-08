e1e09f866da3cfd115b571dca771c19dbee87e06
Sunny George <noisiv807@gmail.com>
2022-04-08 16:07:18 -0700

    My 3rd commit task

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -6,12 +6,12 @@
  * Return: Always 0 (Sucess/correct)
  */
 int main(void)
-  
+
 {
 char lower_case;
 char upper_case;
 
-for (lower_case ='a'; lower_case <= 'z'; lower_case++)
+for (lower_case = 'a'; lower_case <= 'z'; lower_case++)
 {
 putchar(lower_case);
 }
