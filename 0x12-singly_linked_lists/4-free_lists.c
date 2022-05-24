209388715e85a9144b630e8a09a5a4288c0be7a3
Sunny George <noisiv807@gmail.com>
2022-05-24 13:56:45 -0700

    my commit

---
+++ 0x12-singly_linked_lists/4-free_lists.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * free_list - function that frees a list_t list
+ * @head: pointer to the struct list_t
+ *
+ * Return: void
+ */
+
+void free_list(list_t *head)
+{
+	list_t *temp;
+
+	while (head != NULL)
+	{
+		temp = head;
+		head = head->next;
+		free(temp->str);
+		free(temp);
+	}
+}
+
