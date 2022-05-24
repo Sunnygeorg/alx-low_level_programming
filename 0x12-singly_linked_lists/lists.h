8f281846b9f966b23c2a2e8789c2b56c34805842
Sunny George <noisiv807@gmail.com>
2022-05-24 13:02:29 -0700

    lists.h

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -6,6 +6,10 @@
 #include <string.h>
 #include <stdarg.h>
 
+int _putchar(char c);
+char *_strdup(const char *str);
+int _strlen(const char *s);
+
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
@@ -15,6 +19,7 @@
  * Description: singly linked list node structure
  * for Alx project
  */
+
 typedef struct list_s
 {
     char *str;
@@ -27,6 +32,7 @@ size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
+void print_before_main(void);
 
 #endif
 
