f24e22b983a5ca901ff5660460db34592f16cd79
Sunny George <noisiv807@gmail.com>
2022-07-28 11:39:02 -0700

    Task commit

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -11,6 +11,6 @@ unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
   unsigned long int result;
 
-  result = hash_hash_djb2(key) % size;
+  result = hash_djb2(key) % size;
   return (result);
 }
